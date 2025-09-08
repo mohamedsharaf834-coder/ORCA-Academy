fe85f8113eceb8240c8b1a46580d51916c4b688a
mohamed <mohamedsharaf834@gmail.com>
2025-09-09 01:07:14 +0300

    logo

--- src/Home.js
+++ src/Home.js
@@ -15,9 +15,8 @@ function Home({ onStart }) {
         padding: "20px",
       }}
     >
-      <img
-        src="ORCA2.png"
-        alt="ORCA2 Logo"
+     <img src="/ORCA2.png" alt="ORCA Logo" />
+
         style={{
           width: "150px",
           marginBottom: "20px",
